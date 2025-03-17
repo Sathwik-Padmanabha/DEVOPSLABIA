69c3a8ca9e4a0f3718b704d9170e8633f9c52156
Sathwik-Padmanabha <151289571+Sathwik-Padmanabha@users.noreply.github.com>
2025-03-17 14:58:44 +0530

    Area.java

--- Area.java
+++ Area.java
@@ -3,7 +3,7 @@ public class Area{
 	public static void main(String args[]){
 		Scanner sc = new Scanner(System.in);
 		int r;
-		int Ar;
+		double Ar;
 		System.out.println("Enter the Radius:");
 		r = sc.nextInt();
 		Ar = (3.14)*r*r;
