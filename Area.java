665f73fe9f0ba4f11b2cf0ac6702f414c0dbaa43
sudeep <vvce22cseaiml0124@vvce.ac.in>
2025-03-17 14:42:21 +0530

    First commit of the day

---
+++ Area.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class Area{
+	public static void main(String args[]){
+		Scanner sc = new Scanner(System.in);
+		int r;
+		int Ar;
+		System.out.println("Enter the Radius:");
+		r = sc.nextInt();
+		Ar = (3.14)*r*r;
+		System.out.println("Area of Circle:",Ar);
+	}
+}
